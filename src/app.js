4eb4f88c019be793c282c4e1855a70a09e2294f1
Klimm Ivan <ivanklimm87@gmail.com>
2022-07-15 11:27:43 +0300

    feat: Added initial settings

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/', (req, res, next) => {
+  if (req.originalUrl === '/') {
+    res.send('Service is running!');
+    return;
+  }
+  next();
+});
+
+module.exports = app;
---
+++ src/common/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  AUTH_MODE: process.env.AUTH_MODE === 'true',
+  MONGO_DATABASE: process.env.MONDO_DATABASE
+};
