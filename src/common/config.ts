ad6fe90cd2a43ca24a6a1f73677bc7ea8d2f5b36
Klimm Ivan <ivanklimm87@gmail.com>
2022-07-19 14:18:03 +0300

    refactor: Fixed configs

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,25 +1,11 @@
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// export default {
-//   PORT: process.env.PORT,
-//   NODE_ENV: process.env.NODE_ENV,
-//   MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
-//   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-//   AUTH_MODE: process.env.AUTH_MODE === 'true',
-//   MONGO_DATABASE: process.env.MONDO_DATABASE,
-// };
 import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({
-  path: path.join(__dirname, '../../.env')
-});
+dotenv.config();
 
 export default {
   PORT: process.env['PORT'],
   NODE_ENV: process.env['NODE_ENV'],
   MONGO_CONNECTION_STRING: process.env['MONGO_CONNECTION_STRING'],
   JWT_SECRET_KEY: process.env['JWT_SECRET_KEY'],
-  AUTH_MODE: process.env['AUTH_MODE'] === 'true'
+  AUTH_MODE: process.env['AUTH_MODE'] === 'true',
+  MONGO_DATABASE: process.env['MONDO_DATABASE'],
 };
