ea2155354f7d0c1a8b20372c10bf6cde7b2757ff
Klimm Ivan <ivanklimm87@gmail.com>
2022-07-19 09:48:49 +0300

    refactor: Fixed settings tsconfig

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import { connectToDB } from './common/myDB';
 import app from './app';
 
 connectToDB(() => {
-  app.listen(config.PORT, () => console.log(`App is running on http://localhost:${config.PORT}`));
+app.listen(config.PORT, () => console.log(`App is running on http://localhost:${config.PORT}`));
 });
